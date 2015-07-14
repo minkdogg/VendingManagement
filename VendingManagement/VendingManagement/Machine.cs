7bd0eed8af1306f67d9fb5e5ac41a5251cd80697
rroethle <rroethle_he@dev.bse.edu>
2015-07-14 16:35:03 -0500

    Added initial methods for machine class

--- VendingManagement/VendingManagement/Machine.cs
+++ VendingManagement/VendingManagement/Machine.cs
@@ -8,5 +8,133 @@ namespace VendingManagement
 {
     class Machine
     {
+        public List<Product> products = new List<Product> { };
+        
+        protected int maxCapacity;
+        protected float salePrice;
+        protected int minStockItem;
+        protected string city;
+        protected string location;
+        protected bool needService;
+        protected bool needRestock;
+        protected int machineNum;
+
+        public int MaxCapacity
+        {
+            get{ return maxCapacity; }
+            set{ maxCapacity = value; }
+        }
+
+        public float SalePrice
+        {
+            get { return salePrice; }
+            set { salePrice = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public bool NeedService
+        {
+            get { return NeedService; }
+            set { needService = value; }
+        }
+
+        public bool NeedRestock
+        {
+            get { return NeedRestock; }
+            set { needRestock = value; }
+        }
+
+        public bool addProduct(Product product)
+        {
+            if (products.Count() < maxCapacity)
+            {
+                products.Add(product);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool removeProduct(Product product)
+        {
+            return products.Remove(product);
+        }
+
+        public int checkAllQuantity()
+        {
+            int countProduct = products.Count();
+            return countProduct;
+        }
+
+        public int minStock
+        {
+            get { return minStock; }
+            set { minStock = value; }
+        }
+
+        public int MachineNum
+        {
+            get { return machineNum; }
+            set { machineNum = value; }
+        }
+
+
+
+        public List<string> getProductNames()
+        {
+            List<string> productNames = new List<string> { };
+
+            foreach (Product product in products)
+            {
+                productNames.Add(product.Name);
+            }
+
+            return productNames;
+        }
+
+        public List<string> checkItemsRestock()
+        {
+            
+            List<string> restockItems = new List<string> {};
+            List<string> productNames = getProductNames();
+            List<string> uniqueProductNames = productNames.Distinct().ToList();
+
+            var groupProduct = productNames.GroupBy(i => i);
+
+            foreach (var group in groupProduct)
+            {
+                
+                restockItems.Add(group.Key);
+            }
+
+            return restockItems;
+        }
+
+       
+
+
+
+        public Machine(string city, string location)
+        {
+            this.city = city;
+            this.location = location;
+            this.machineNum = 1;
+        }
+
+
+
     }
 }
