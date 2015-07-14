cff5ac96f0853fa497ab2670d04049b534a4b52d
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-07-14 13:16:19 -0500

    Database Stub

---
+++ VendingManagement/VendingManagement/Database.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingManagement
+{
+    class Database
+    {
+        List<Product> listProduct = new List<Product>;
+        List<Machine> listMachine = new List<Machine>;
+        List<City> listCity = new List<City>;
+        List<Employee> listEmployee = new  List<Employee>;
+        List<Vehicle> listVehicle = new List<Vehicle>;
+        List<Transaction> listTransaction = new List<Transaction>;
+
+        
+
+        public void generateInitialData()
+        {
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
