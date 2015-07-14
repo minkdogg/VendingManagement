0b895a31637b8ad5fd8c6829b91a1be470a00556
rroethle <rroethle_he@dev.bse.edu>
2015-07-14 13:39:38 -0500

    Product class updated with name and wholesaleprice

--- VendingManagement/VendingManagement/Product.cs
+++ VendingManagement/VendingManagement/Product.cs
@@ -8,5 +8,39 @@ namespace VendingManagement
 {
     class Product
     {
+
+        protected string name;
+        protected float wholeSalePrice;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public float WholeSalePrice
+        {
+            get
+            {
+                return wholeSalePrice;
+            }
+            set
+            {
+                wholeSalePrice = value;
+            }
+        }
+
+        public Product(string name, float wholeSalePrice)
+        {
+            this.name = name;
+            this.wholeSalePrice = wholeSalePrice;
+        }
+
     }
 }
