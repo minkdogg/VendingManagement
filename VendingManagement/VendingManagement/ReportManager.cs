796cdd516fb2ded29e9111ddf1e4b6f2d2a93b61
rroethle <rroethle_he@dev.bse.edu>
2015-07-16 16:05:12 -0500

    Added Revenue By City, business inventory report and items by machine report. Updated database to transfer to additional machines.

--- VendingManagement/VendingManagement/ReportManager.cs
+++ VendingManagement/VendingManagement/ReportManager.cs
@@ -12,6 +12,37 @@ namespace VendingManagement
 {
     class ReportManager
     {
+
+        public DataTable reportProductWarehouse(List<Product>data)
+        {
+            DataTable table = new DataTable();
+            DataColumn newColumn = new DataColumn("Product Type", typeof(String));
+            table.Columns.Add(newColumn);
+
+            newColumn = new DataColumn("Number of Items", typeof(String));
+            table.Columns.Add(newColumn);
+
+            newColumn = new DataColumn("Wholesale Cost", typeof(String));
+            table.Columns.Add(newColumn);
+
+            List<Product> sortedList = data.OrderBy(d => d.Name).ToList();
+            var subtotals = from x in sortedList
+                            group x by x.name into g
+                            select new { Type = g.Key, Count = g.Count(), Price = g.Sum(x => x.WholeSalePrice) };
+
+            foreach (var total in subtotals)
+            {
+                DataRow newRow = table.NewRow();
+                newRow["Product Type"] = total.Type;
+                newRow["Number of Items"] = total.Count;
+                newRow["Wholesale Cost"] = total.Price;
+                table.Rows.Add(newRow);
+            }
+
+            return table;
+            
+        }
+        
         public DataTable reportSalesByItem(List<Transactions> data)
         {
 
@@ -212,6 +243,106 @@ namespace VendingManagement
             return table;
         }
 
+
+        public DataTable reportProductByMachine(List<Machine> machines)
+        {
+
+            DataTable table = new DataTable();
+            DataColumn newColumn = new DataColumn("Machine ID", typeof(String));
+            table.Columns.Add(newColumn);
+
+            newColumn = new DataColumn("Product Type", typeof(String));
+            table.Columns.Add(newColumn);
+
+            newColumn = new DataColumn("Product Count", typeof(String));
+            table.Columns.Add(newColumn);
+
+
+            foreach (Machine machine in machines)
+            {
+                table.Rows.Add(machine.MachineID, "", "");
+                List<Product> machineProducts = machine.products;
+                List<Product> sortedList = machineProducts.OrderBy(d => d.Name).ToList();
+                var subtotals = from x in sortedList
+                                group x by x.Name into g
+                                select new { Type = g.Key, SalesCount = g.Count() };
+                foreach (var total in subtotals)
+                {
+                  
+                        table.Rows.Add("", total.Type, total.SalesCount);
+                }
+                
+                table.Rows.Add("", "", "");
+            }
+
+            return table;
+        }
+
+
+        public DataTable reportRevenueByCity(List<Transactions> data, List<Machine> machines, List<City> cities)
+        {
+
+            DataTable table = new DataTable();
+            DataColumn newColumn = new DataColumn("City", typeof(String));
+            table.Columns.Add(newColumn);
+
+            newColumn = new DataColumn("Sales Profit", typeof(String));
+            table.Columns.Add(newColumn);
+
+            newColumn = new DataColumn("Sales Total", typeof(String));
+            table.Columns.Add(newColumn);
+
+            newColumn = new DataColumn("Sales Count", typeof(String));
+            table.Columns.Add(newColumn);
+
+
+
+
+            foreach (City city in cities)
+            {
+                table.Rows.Add(city.Name);
+                List<string> cityMachineList = new List<string> { };
+                foreach (Machine machine in machines)
+                {
+                    if (machine.City == city.Name)
+                    {
+                        cityMachineList.Add(machine.MachineID);
+
+                    }
+                }
+
+                float salesTotalProfit = 0;
+                float salesTotalAmount = 0;
+                int salesTotalCount = 0;
+                foreach (string machine in cityMachineList)
+                {
+
+                    List<Transactions> sortedList = data.OrderBy(d => d.ProductID).ToList();
+                    var filteredItems = sortedList.Where(p => p.Account == machine);
+
+                    var subtotals = from x in filteredItems
+                                    group x by x.Account into g
+                                    select new { Type = g.Key, SalesProfit = g.Where(x => x.Account == machine).Select(x => x.Amount).Sum(), SalesTotal = g.Where(x => x.Amount >= 0 && x.Account == machine).Select(x => x.Amount).Sum(), SalesCount = g.Where(x => x.Amount >= 0 && x.Account == machine).Count() };
+
+                    foreach (var total in subtotals)
+                    {
+                        if (total.Type != "Checking")
+                        {
+                            table.Rows.Add("", total.SalesProfit, total.SalesTotal, total.SalesCount);
+                        }
+                        salesTotalProfit += total.SalesProfit;
+                        salesTotalAmount += total.SalesTotal;
+                        salesTotalCount += total.SalesCount;
+                    }
+
+                }
+                table.Rows.Add("Total", salesTotalProfit, salesTotalAmount, salesTotalCount);
+                table.Rows.Add("", "", "", "");
+
+            }
+
+            return table;
+        }
 
 
 
