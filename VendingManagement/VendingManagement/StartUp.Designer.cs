a6aa36e4403b52b3663ccbad5cd87c2f3c76e487
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-07-16 10:28:47 -0500

    Added Remove Employee Logic

--- VendingManagement/VendingManagement/StartUp.Designer.cs
+++ VendingManagement/VendingManagement/StartUp.Designer.cs
@@ -585,6 +585,7 @@
             this.button5.TabIndex = 1;
             this.button5.Text = "Remove";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // EmployeesDataGrid
             // 
