ca6ac9df5134cbc6f93352bb0ff4c949a3816757
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-07-16 12:49:36 -0500

    Added Grid Refresh Methods

--- VendingManagement/VendingManagement/StartUp`2.cs
+++ VendingManagement/VendingManagement/StartUp`2.cs
@@ -12,7 +12,9 @@ namespace VendingManagement
 {
     public partial class StartUp : Form
     {
-
+        //
+        // Load/Refresh Full Datagrid views
+        //
         public void LoadProductWarehouseDataGrid()
         {
             ProductWarehouseDataGrid.DataSource = null;
@@ -24,6 +26,19 @@ namespace VendingManagement
             ProductWarehouseDataGrid.DataSource = data;
             ProductWarehouseDataGrid.Update();
             ProductWarehouseDataGrid.Refresh();
+        }
+                
+        public void LoadTransactionsDataGrid()
+        {
+            TransactionsDataGrid.DataSource = null;
+            TransactionsDataGrid.Update();
+            TransactionsDataGrid.Refresh();
+
+            List<Transactions> data;
+            data = this.database.SelectAllTransactions();
+            TransactionsDataGrid.DataSource = data;
+            TransactionsDataGrid.Update();
+            TransactionsDataGrid.Refresh();
         }
 
         public void LoadEmployeesDataGrid()
@@ -38,6 +53,110 @@ namespace VendingManagement
             EmployeesDataGrid.Update();
             EmployeesDataGrid.Refresh();
         }
+
+        public void LoadVehiclesDataGrid()
+        {
+            VehiclesDataGrid.DataSource = null;
+            VehiclesDataGrid.Update();
+            VehiclesDataGrid.Refresh();
+
+            List<Vehicle> data;
+            data = this.database.SelectAllVehicle();
+            VehiclesDataGrid.DataSource = data;
+            VehiclesDataGrid.Update();
+            VehiclesDataGrid.Refresh();
+        }
+        
+        public void LoadCityMachinesDataGrid()
+        {
+            CityMachinesDataGrid.DataSource = null;
+            CityMachinesDataGrid.Update();
+            CityMachinesDataGrid.Refresh();
+
+            List<Machine> data;
+            data = this.database.SelectAllMachine();
+            CityMachinesDataGrid.DataSource = data;
+            CityMachinesDataGrid.Update();
+            CityMachinesDataGrid.Refresh();
+        }
+
+        public void LoadCityTotalRevenueDataGrid()
+        {
+            CityTotalRevenueDataGrid.DataSource = null;
+            CityTotalRevenueDataGrid.Update();
+            CityTotalRevenueDataGrid.Refresh();
+
+            List<Transactions> data;
+            data = this.database.SelectAllTransactions();
+            CityTotalRevenueDataGrid.DataSource = data;
+            CityTotalRevenueDataGrid.Update();
+            CityTotalRevenueDataGrid.Refresh();
+        }
+
+        public void LoadCitySalesByItemDataGrid()
+        {
+            CitySalesByItemDataGrid.DataSource = null;
+            CitySalesByItemDataGrid.Update();
+            CitySalesByItemDataGrid.Refresh();
+
+            List<Transactions> data;
+            data = this.database.SelectAllTransactions();
+            CitySalesByItemDataGrid.DataSource = data;
+            CitySalesByItemDataGrid.Update();
+            CitySalesByItemDataGrid.Refresh();
+        }
+
+        public void LoadMachineListAllDataGrid()
+        {
+            MachineListAllDataGrid.DataSource = null;
+            MachineListAllDataGrid.Update();
+            MachineListAllDataGrid.Refresh();
+
+            List<Machine> data;
+            data = this.database.SelectAllMachine();
+            MachineListAllDataGrid.DataSource = data;
+            MachineListAllDataGrid.Update();
+            MachineListAllDataGrid.Refresh();
+        }
+
+        public void LoadMachineTypeDataGrid()
+        {
+            MachineTypeDataGrid.DataSource = null;
+            MachineTypeDataGrid.Update();
+            MachineTypeDataGrid.Refresh();
+
+            List<Machine> data;
+            data = this.database.SelectAllMachine();
+            MachineTypeDataGrid.DataSource = data;
+            MachineTypeDataGrid.Update();
+            MachineTypeDataGrid.Refresh();
+        }
+
+        public void LoadMachineProductItemsDataGrid()
+        {
+            MachineProductItemsDataGrid.DataSource = null;
+            MachineProductItemsDataGrid.Update();
+            MachineProductItemsDataGrid.Refresh();
+
+            List<Machine> data;
+            data = this.database.SelectAllMachine();
+            MachineProductItemsDataGrid.DataSource = data;
+            MachineProductItemsDataGrid.Update();
+            MachineProductItemsDataGrid.Refresh();
+        }
+
+
+
+
+
+
+            
+
+
+
+
+
+
 
     }
 }
